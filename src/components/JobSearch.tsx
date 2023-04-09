8471e74011427e217f6e051aabf5f70f3cf29064
Gregory Seidman <34869611+gregory-seidman@users.noreply.github.com>
2023-04-09 17:45:07 -0400

    job editing works

--- src/components/JobSearch.tsx
+++ src/components/JobSearch.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { connect } from "react-redux";
 import ReduxState from "../state/ReduxState";
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { ColDef, RowsProp } from "@material-ui/data-grid";
+import { ColDef, RowData, RowsProp } from "@material-ui/data-grid";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
+import Edit from "@material-ui/icons/Edit";
+import List from "@material-ui/icons/List";
 import { Job, JobSearch } from "../models";
 import rowBuilder from "./rowBuilder";
 import columnBuilder from "./columnBuilder";
-import TableGrid from "./TableGrid";
+import TableGrid, { ColumnButtonDefs } from "./TableGrid";
 import EditJobDialog from "./EditJobDialog";
 
 interface InputPropsType {
@@ -43,22 +45,46 @@ const mapStateAndProps: Mapper = (state, props) => {
 const ComponentFunc: React.FC<MappedPropsType> = ({ jobSearch }) => {
     const { fieldNames, fieldOrder, jobs } = jobSearch;
     const jobsList: Job[] = Object.values(jobs);
-    const [ curJobId, setCurJobId ] = React.useState("");
+    const [ curJobId, setCurJobId_ ] = React.useState("");
+    const [ creatingNew, setCreatingNew ] = React.useState(false);
     const currentJob: Job|undefined = jobs[curJobId];
-    const open: boolean = !!(curJobId || (jobsList.length === 0));
+    const openEdit: boolean = !!(creatingNew || curJobId || (jobsList.length === 0));
     const classes = useStyles();
     const columns: ColDef[] = fieldOrder.map(columnBuilder(fieldNames));
     const rows: RowsProp = jobsList.map(rowBuilder(fieldOrder));
+    const setCurJobId = (id: string) => {
+        setCurJobId_(id);
+        setCreatingNew(false);
+    }
+    const buttons: ColumnButtonDefs = [
+        {
+            field: "_lastInteractionDate",
+            button: <List />,
+            onClick: (row: RowData) => {} //TODO
+        },
+        {
+            field: "_nextInteractionDate",
+            button: <AddIcon />,
+            onClick: (row: RowData) => {} //TODO
+        },
+        {
+            field: "company",
+            button: <Edit />,
+            onClick: (row: RowData) => { setCurJobId(row.id.toString())}
+        }
+    ];
     const closeDialog = () => setCurJobId("");
+
     return (
         <React.Fragment>
             <div className={classes.gridWrapper}>
                 <TableGrid
                     rows={rows}
                     columns={columns}
+                    buttons={buttons}
                 />
                 <Fab
-                    onClick={() => setCurJobId("      ")}
+                    onClick={() => setCreatingNew(true)}
                     className={classes.fab}
                     color="primary"
                     aria-label="add"
@@ -66,12 +92,14 @@ const ComponentFunc: React.FC<MappedPropsType> = ({ jobSearch }) => {
                     <AddIcon />
                 </Fab>
             </div>
+            { openEdit ? (
             <EditJobDialog
                 job={currentJob}
-                open={open}
+                open={openEdit}
                 disableCancel={!jobsList.length}
                 closeDialog={closeDialog}
             />
+            ) : null }
         </React.Fragment>
     );
 }
